875dece98de9ead9ae804034a074e558ab8b3936
Prafull Jain <32505550+PrafullJain@users.noreply.github.com>
2020-12-21 17:36:37 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ var http = require('http');
 const fs = require('fs')
 const port = process.env.PORT || 3000
 const request = require('request');
+const axios = require('axios');
+
 app.get("*", (req, res) => {
 	const ind = path.join(__dirname, 'public', 'index.html');
 	res.sendFile(ind);
@@ -22,7 +24,33 @@ app.use(bodyParser.json());
 app.use(bodyParser());
 
 app.post('/PostData', (req, res) => {
-			const clientSec = req.body.clientSecret
+			const clientSec = req.body.clientSecret;
+			const clientId = req.body.clientId;
+	var postData = {
+  'client_id': '3pj0hhotbu0t2c62lrzib020', //pass Client ID
+					'client_secret': '82Z6UD4Zos80P1G6csqsrdaq', //pass Client Secret
+					'grant_type': 'client_credentials',
+					'account_id': '514010252'
+};
+
+	
+	let axiosConfig = {
+  headers: {
+					'content-type': 'application/json'
+				}
+};
+	
+axios
+  .post('https://mc6vgk-sxj9p08pqwxqz9hw9-4my.auth.marketingcloudapis.com/v2/token',postData,axiosConfig )
+  .then(res => {
+    console.log(`statusCode: ${res.statusCode}`)
+    console.log(res)
+  })
+  .catch(error => {
+    console.error(error)
+  })
+
+	/*
 			request.post({
 				headers: {
 					'content-type': 'application/json'
@@ -37,41 +65,7 @@ app.post('/PostData', (req, res) => {
 				json: true
 			}, function(error, response, body) {
 				const data='<s:Envelope xmlns:s="http://www.w3.org/2003/05/soap-envelope" xmlns:a="http://schemas.xmlsoap.org/ws/2004/08/addressing" xmlns:u="http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-utility-1.0.xsd"><s:Header><a:Actions:mustUnderstand="1">Retrieve</a:Action><a:MessageID>urn:uuid:7e0cca04-57bd-4481-864c-6ea8039d2ea0</a:MessageID><a:ReplyTo>       <a:Address>http://schemas.xmlsoap.org/ws/2004/08/addressing/role/anonymous</a:Address></a:ReplyTo><a:To s:mustUnderstand="1">https://mc6vgk-sxj9p08pqwxqz9hw9-4my.soap.marketingcloudapis.com/Service.asmx</a:To>      <fueloauth xmlns="http://exacttarget.com">'+body.access_token+'</fueloauth></s:Header><s:Body xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema">     <RetrieveRequestMsg xmlns="http://exacttarget.com/wsdl/partnerAPI"> <RetrieveRequest><ObjectType>DataExtension</ObjectType> <Properties>CustomerKey</Properties><Properties>Name</Properties><Properties>DataExtension.ObjectID</Properties> <Properties>IsSendable</Properties>  <Properties>CategoryID</Properties>  	<Filter xsi:type="SimpleFilterPart"> <Property>CategoryID</Property><SimpleOperator>equals</SimpleOperator><Value>29130</Value></Filter>    </RetrieveRequest> </RetrieveRequestMsg> </s:Body></s:Envelope>';
-				/*
-				request.post({
-				headers: {
-					'content-type': 'text/xml',
-					'Authorization':'Bearer '+body.access_token,
-					SOAPAction: 'runTransaction'
-				},
-				url: 'https://mc6vgk-sxj9p08pqwxqz9hw9-4my.soap.marketingcloudapis.com/Service.asmx',
-				body:data,
-				json:true
-				},function(error2,response2,body2){
-					
-					console.log('\nSOAP Body'+(body));
-					console.log('\nSOAP Error'+(error2));
-					console.log('\nSOAP Response'+(response2));
-					console.log('\nSOAP Body'+JSON.stringify(body));
-					console.log('\nSOAP Error'+JSON.stringify(error2));
-					console.log('\nSOAP Response'+JSON.stringify(response2));
-					
-				})	
-				*/
-				
-				const opts = {
-    body: data,
-    headers: {
-        'Content-Type': 'text/xml; charset=utf-8',
-        SOAPAction: 'runTransaction'
-    }
-}
 
-const url = 'https://mc6vgk-sxj9p08pqwxqz9hw9-4my.soap.marketingcloudapis.com/Service.asmx'
-
-const xbod = request.post(url, opts, (err, response) => {
-    console.log('response', response.xbod)
-})
 				const ind2 = path.join(__dirname, 'public', 'SFMC-DE.html');
 				res.sendFile(ind2);
 				console.log("Access" + body.access_token);
@@ -79,8 +73,10 @@ const xbod = request.post(url, opts, (err, response) => {
 				console.log(clientSec);
 				//  res.send(clientSec);   
 			})
+			*/
 	
 });
+
 app.listen(port, () => {
       console.log('Example app is listening on port http://localhost:${port}');
 });
