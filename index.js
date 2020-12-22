f2fe5b76567d5dd92ca9e4bc2b10c58a19d31c95
Prafull Jain <32505550+PrafullJain@users.noreply.github.com>
2020-12-22 17:14:00 +0530

    Update index.js

--- index.js
+++ index.js
@@ -13,50 +13,50 @@ let xml = '';
 const soapURL = 'https://mc6vgk-sxj9p08pqwxqz9hw9-4my.soap.marketingcloudapis.com/Service.asmx'
 
 app.get("*", (req, res) => {
-	const ind = path.join(__dirname, 'public', 'index.html');
-	res.sendFile(ind);
+    const ind = path.join(__dirname, 'public', 'index.html');
+    res.sendFile(ind);
 });
 app.use(express.urlencoded({
-	extended: true
+    extended: true
 }))
 var bodyParser = require('body-parser');
 // configure app to use bodyParser()
 // this will let us get the data from a POST
 app.use(bodyParser.urlencoded({
-	extended: true
+    extended: true
 }));
 app.use(bodyParser.json());
 app.use(bodyParser());
 
-const getToken = async (clientid, clientSecret, url) => {
-   var formData = new FormData();
-   formData.append('client_id', clientid)
-   formData.append('client_secret', clientSecret)
-   formData.append('grant_type', 'client_credentials')
-   const tokenResponse = await axios({
-      method: 'post',
-      url: `${url}v2/token`,
-      data: formData,
-      headers: {
-         'Content-Type': `multipart/form-data; boundary=${formData._boundary}`,
-      },
-   })
-   return tokenResponse.data.access_token
+const getToken = async(clientid, clientSecret, url) => {
+    var formData = new FormData();
+    formData.append('client_id', clientid)
+    formData.append('client_secret', clientSecret)
+    formData.append('grant_type', 'client_credentials')
+    const tokenResponse = await axios({
+        method: 'post',
+        url: `${url}v2/token`,
+        data: formData,
+        headers: {
+            'Content-Type': `multipart/form-data; boundary=${formData._boundary}`,
+        },
+    })
+    return tokenResponse.data.access_token
 }
 
 app.post('/PostData', (req, res) => {
-	try {
-		const { clientId, clientSecret, url } = req.body;
-		console.log(clientId);
-		console.log(clientSecret);
-		console.log(url);
-		
-		(async function(){
+    try {
+        const { clientId, clientSecret, url } = req.body;
+        console.log(clientId);
+        console.log(clientSecret);
+        console.log(url);
 
-      const token = await getToken(clientId, clientSecret, url);
-			console.log(token);
-			
-			const soapData = `<?xml version="1.0" encoding="UTF-8"?>
+        (async function() {
+
+            const token = await getToken(clientId, clientSecret, url);
+            console.log(token);
+
+            const soapData = `<?xml version="1.0" encoding="UTF-8"?>
       <s:Envelope xmlns:s="http://www.w3.org/2003/05/soap-envelope" xmlns:a="http://schemas.xmlsoap.org/ws/2004/08/addressing" xmlns:u="http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-utility-1.0.xsd">
          <s:Header>
             <a:Action s:mustUnderstand="1">Retrieve</a:Action>
@@ -88,76 +88,74 @@ app.post('/PostData', (req, res) => {
             </RetrieveRequestMsg>
          </s:Body>
       </s:Envelope>`
-      const soapResponse = await axios({
-         method: 'post',
-         headers: {
-            'Content-Type': 'text/xml',
-            'Authorization': `Bearer ${token}`
-         },
-         url: soapURL,
-         data: soapData,
-         timeout: 10000
-      })
-      if (soapResponse) {
-         parser.parseString(soapResponse.data,function(err,result){
-            console.dir('dir results : ',result);
-            console.log('log results : ',JSON.stringify(result));
-         })
-         res.send({
-           // data: soapResponse.data
-         }
-         )
-			
-			
-			
-			
-			
-			
-			
-			
-		})();
-
-			
-	
-	const ind2 = path.join(__dirname, 'public', 'SFMC-DE.html');
-					console.log(token);
-
-				res.sendFile(ind2);
-				//console.log("Access" + body.access_token);
-				//console.log("response" + response);
-				//console.log(clientSec);
+            const soapResponse = await axios({
+                method: 'post',
+                headers: {
+                    'Content-Type': 'text/xml',
+                    'Authorization': `Bearer ${token}`
+                },
+                url: soapURL,
+                data: soapData,
+                timeout: 10000
+            })
+            if (soapResponse) {
+                parser.parseString(soapResponse.data, function(err, result) {
+                    console.dir('dir results : ', result);
+                    console.log('log results : ', JSON.stringify(result));
+                })
+                res.send({
+                    // data: soapResponse.data
+                })
 
 
-	/*
-			request.post({
-				headers: {
-					'content-type': 'application/json'
-				},
-				url: 'https://mc6vgk-sxj9p08pqwxqz9hw9-4my.auth.marketingcloudapis.com/v2/token',
-				body: {
-					'client_id': '3pj0hhotbu0t2c62lrzib020', //pass Client ID
-					'client_secret': '82Z6UD4Zos80P1G6csqsrdaq', //pass Client Secret
-					'grant_type': 'client_credentials',
-					'account_id': '514010252'
-				},
-				json: true
-			}, function(error, response, body) {
-				const data='<s:Envelope xmlns:s="http://www.w3.org/2003/05/soap-envelope" xmlns:a="http://schemas.xmlsoap.org/ws/2004/08/addressing" xmlns:u="http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-utility-1.0.xsd"><s:Header><a:Actions:mustUnderstand="1">Retrieve</a:Action><a:MessageID>urn:uuid:7e0cca04-57bd-4481-864c-6ea8039d2ea0</a:MessageID><a:ReplyTo>       <a:Address>http://schemas.xmlsoap.org/ws/2004/08/addressing/role/anonymous</a:Address></a:ReplyTo><a:To s:mustUnderstand="1">https://mc6vgk-sxj9p08pqwxqz9hw9-4my.soap.marketingcloudapis.com/Service.asmx</a:To>      <fueloauth xmlns="http://exacttarget.com">'+body.access_token+'</fueloauth></s:Header><s:Body xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema">     <RetrieveRequestMsg xmlns="http://exacttarget.com/wsdl/partnerAPI"> <RetrieveRequest><ObjectType>DataExtension</ObjectType> <Properties>CustomerKey</Properties><Properties>Name</Properties><Properties>DataExtension.ObjectID</Properties> <Properties>IsSendable</Properties>  <Properties>CategoryID</Properties>  	<Filter xsi:type="SimpleFilterPart"> <Property>CategoryID</Property><SimpleOperator>equals</SimpleOperator><Value>29130</Value></Filter>    </RetrieveRequest> </RetrieveRequestMsg> </s:Body></s:Envelope>';
+            }
 
-				const ind2 = path.join(__dirname, 'public', 'SFMC-DE.html');
-				res.sendFile(ind2);
-				console.log("Access" + body.access_token);
-				console.log("response" + response);
-				console.log(clientSec);
-				//  res.send(clientSec);   
-			})
-			*/
-		  } catch (err) {
-      console.log(err);
-   }
-	
+
+
+
+
+        })();
+
+
+
+        const ind2 = path.join(__dirname, 'public', 'SFMC-DE.html');
+        console.log(token);
+
+        res.sendFile(ind2);
+        //console.log("Access" + body.access_token);
+        //console.log("response" + response);
+        //console.log(clientSec);
+
+
+        /*
+        		request.post({
+        			headers: {
+        				'content-type': 'application/json'
+        			},
+        			url: 'https://mc6vgk-sxj9p08pqwxqz9hw9-4my.auth.marketingcloudapis.com/v2/token',
+        			body: {
+        				'client_id': '3pj0hhotbu0t2c62lrzib020', //pass Client ID
+        				'client_secret': '82Z6UD4Zos80P1G6csqsrdaq', //pass Client Secret
+        				'grant_type': 'client_credentials',
+        				'account_id': '514010252'
+        			},
+        			json: true
+        		}, function(error, response, body) {
+        			const data='<s:Envelope xmlns:s="http://www.w3.org/2003/05/soap-envelope" xmlns:a="http://schemas.xmlsoap.org/ws/2004/08/addressing" xmlns:u="http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-utility-1.0.xsd"><s:Header><a:Actions:mustUnderstand="1">Retrieve</a:Action><a:MessageID>urn:uuid:7e0cca04-57bd-4481-864c-6ea8039d2ea0</a:MessageID><a:ReplyTo>       <a:Address>http://schemas.xmlsoap.org/ws/2004/08/addressing/role/anonymous</a:Address></a:ReplyTo><a:To s:mustUnderstand="1">https://mc6vgk-sxj9p08pqwxqz9hw9-4my.soap.marketingcloudapis.com/Service.asmx</a:To>      <fueloauth xmlns="http://exacttarget.com">'+body.access_token+'</fueloauth></s:Header><s:Body xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema">     <RetrieveRequestMsg xmlns="http://exacttarget.com/wsdl/partnerAPI"> <RetrieveRequest><ObjectType>DataExtension</ObjectType> <Properties>CustomerKey</Properties><Properties>Name</Properties><Properties>DataExtension.ObjectID</Properties> <Properties>IsSendable</Properties>  <Properties>CategoryID</Properties>  	<Filter xsi:type="SimpleFilterPart"> <Property>CategoryID</Property><SimpleOperator>equals</SimpleOperator><Value>29130</Value></Filter>    </RetrieveRequest> </RetrieveRequestMsg> </s:Body></s:Envelope>';
+        			const ind2 = path.join(__dirname, 'public', 'SFMC-DE.html');
+        			res.sendFile(ind2);
+        			console.log("Access" + body.access_token);
+        			console.log("response" + response);
+        			console.log(clientSec);
+        			//  res.send(clientSec);   
+        		})
+        		*/
+    } catch (err) {
+        console.log(err);
+    }
+
 });
 
 app.listen(port, () => {
-      console.log('Example app is listening on port http://localhost:${port}');
+    console.log('Example app is listening on port http://localhost:${port}');
 });
