8ec1a4cfc3fe81eb2b1ca6e981c85bc255f2ef4c
Prafull Jain <32505550+PrafullJain@users.noreply.github.com>
2020-12-22 17:42:06 +0530

    Update index.js

--- index.js
+++ index.js
@@ -100,9 +100,8 @@ app.post('/PostData', (req, res) => {
             })
             if (soapResponse) {
                 parser.parseString(soapResponse.data, function(err, result) {
-                    console.dir('dir results : ', result);
-                    var data12=JSON.Parse(result);
-                    console.log('log results : ', JSON.stringify(data12['soap:Envelope']['soap:Body']['Results']));
+                    console.dir('dir results : ',  JSON.stringify(result));
+                    console.log('log results : ', JSON.stringify(result['soap:Envelope']['soap:Body']['Results']));
                 })
                 res.send({
                     // data: soapResponse.data
